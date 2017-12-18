987537dd9030b3dca9538944c70d863932527099
Alex <jzsplking@gmail.com>
2017-12-18 22:35:10 +0800

    add the filp js, style for the flip cards

--- scripts/main.js
+++ scripts/main.js
@@ -9,8 +9,10 @@
 })(jQuery);
 
 //define the cards item
-var symbols = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'],
-$deck = $('.deck');
+var symbols = ['1', '1', '2', '2', '3', '3', '4', '4', '5', '5', '6', '6', '7', '7', '8', '8'],
+$deck = $('.deck'),
+opened = [],
+delay = 800;
 
 //initial game
 function initGame() {
@@ -20,3 +22,29 @@ function initGame() {
 		$deck.append($('<li class="card"><i>' + cards[i] + '</i></li>'));
 	}
 }
+
+//flip cards
+$deck.on('click', '.card:not(".match, .open")', function() {
+	var $this = $(this),
+	card = $this.context.innerHTML;
+	$this.addClass('open show');
+	opened.push(card);
+	console.log(opened);
+
+	if(opened.length > 1) {
+		if(card == opened[0]) {
+			$deck.find('.open').addClass('match');
+			setTimeout(function() {
+				$deck.find('.match').removeClass('open show');
+			}, delay);
+		}
+		else {
+			$deck.find('.open').addClass('notmatch');
+			setTimeout(function() {
+				$deck.find('.open').removeClass('open show notmatch');
+			}, delay / 1.5);
+		}
+		opened = [];
+	}
+	
+});
