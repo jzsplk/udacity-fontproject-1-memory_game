b5a23292873808a23cb056e09d0b52883b9d13bc
Alex <jzsplking@gmail.com>
2017-12-21 00:06:22 +0800

    Feature: 添加给游戏结果评比星级评分逻辑， 把实时星级评分显示在面板上

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,26 @@
 
 	// Document ready
 	$(document).ready(function() {
-		console.log("working");
+		console.log("game working");
 		initGame();
 	});
 
 })(jQuery);
+
+//define the cards item
+var symbols = ['1', '1', '2', '2', '3', '3', '4', '4', '5', '5', '6', '6', '7', '7', '8', '8'],
+$deck = $('.deck'),
+$scorePanel = $('.score-panel'),
+$movesNumber = $scorePanel.find('.moves'),
+$ratingStars = $scorePanel.find('i'),
+opened = [],
+delay = 800,
+match = 0,
+moves = 0,
+cardsToMatch = symbols.length / 2,
+rant3stars = cardsToMatch + 2,
+rant2stars = cardsToMatch + 6,
+rant1stars = cardsToMatch + 10;
 
 // make shuffle of array
 function shuffle(array) {
@@ -23,42 +38,54 @@ function shuffle(array) {
 	return array;
 }
 
-
-//define the cards item
-var symbols = ['1', '1', '2', '2', '3', '3', '4', '4', '5', '5', '6', '6', '7', '7', '8', '8'],
-$deck = $('.deck'),
-opened = [],
-delay = 800,
-match = 0,
-moves = 0,
-cardsToMatch = symbols.length / 2;
-
 //initial game
 function initGame() {
 	var cards = shuffle(symbols);
 	$deck.empty();
 	match = 0;
 	moves = 0;
+	$movesNumber.html(moves);
+	$ratingStars.removeClass('fa-star-o').addClass('fa-star');
 	for(var i = 0; i < cards.length; i++) {
 		$deck.append($('<li class="card"><i>' + cards[i] + '</i></li>'));
 	}
 }
 
 //end Game function
-function endGame(moves) {
+function endGame(moves, score) {
 	swal({
 			position: 'center',
 		    type: 'success',
 		    title: '大头棒棒哒！',
-		    text: '用了' + moves + '步',
+		    text: '用了 ' + moves + ' 步,' + '得到 ' + score + '星， ' + ' Winner Winner Chicken Dinner!',
 		    confirmButtonColor: '#9bcb3c',
-		    confirmButtonText: '再来一次',
+		    confirmButtonText: '再来一局',
 	}).then(function(isConfirm) {
 		if(isConfirm) {
 			initGame();
 		}
 	});
 }
+
+//rating stars
+function ratingStars(moves) {
+	var rating = 3;
+	if (moves > rant3stars && moves < rant2stars) {
+		$ratingStars.eq(2).removeClass('fa-star').addClass('fa-star-o');
+		rating = 2;
+	} else if (moves > rant2stars && moves < rant1stars) {
+		$ratingStars.eq(1).removeClass('fa-star').addClass('fa-star-o');
+		rating = 1;
+	} else if (moves > rant1stars) {
+		$ratingStars.eq(0).removeClass('fa-star').addClass('fa-star-o');
+		rating = 0;
+	}
+	console.log(moves);
+	console.log(rating);
+	return { score: rating };
+
+}
+
 
 //flip cards
 $deck.on('click', '.card:not(".match, .open")', function() {
@@ -80,17 +107,21 @@ $deck.on('click', '.card:not(".match, .open")', function() {
 			$deck.find('.open').addClass('notmatch');
 			setTimeout(function() {
 				$deck.find('.open').removeClass('open show notmatch');
-			}, delay / 1.5);
+			}, delay);
 		}
 
 		opened = [];
 		moves++;
+		ratingStars(moves);
+		$movesNumber.html(moves);
 	}
 
 	//end Game if match equal to cardsToMatch
 	if(match === cardsToMatch) {
+		ratingStars(moves);
+		var score = ratingStars(moves).score;
 		setTimeout(function() {
-			endGame(moves);
+			endGame(moves, score);
 		}, 500);
 	}
 	
